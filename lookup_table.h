9d59f59b93d3c62ae4690dbc92562f2231d888fa
Daiji Yamashita <yamashita-daiji@sei.co.jp>
2020-07-22 10:17:07 +0900

    implement linear interpolation

--- lookup_table.h
+++ lookup_table.h
@@ -16,7 +16,6 @@
 // C++14 feature (constexpr initializer list) is required.
 //
 
-#include <utility>
 #include <array>
 #include <vector>
 #include <algorithm>
@@ -38,10 +37,21 @@ public:
 			[](const Entry& a, const Entry& b) { return a[0] < b[0]; }
 		);
 
-		// If it is end(), returns second of the last entry.
-		// Otherwize returns second of the lower bound entry.
-		return (it == _entries.end()) ? (*std::prev(_entries.end()))[1] : (*it)[1];
+		// If "it" is end(), returns the last value
+		if (it == _entries.end()) return (*std::prev(_entries.end()))[1];
+		
+		// It "it" is begin(), returns the first value
+		if (it == _entries.begin()) return (*it)[1];
 
+		// Calculate linear interpolation
+		auto it_prev = std::prev(it);
+		
+		const double &x0 = (*it_prev)[0];
+		const double &x1 = (*it)[0];
+		const double &y0 = (*it_prev)[1];
+		const double &y1 = (*it)[1];
+
+		return (y1 - y0) * (x - x0) / (x1 - x0) + y0;
 	}
 
 private:
